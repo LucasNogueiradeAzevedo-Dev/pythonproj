56ff190ba74bda4875196b2b9ba610223f87e7e6
Lucas Nogueira de Azevedo <115651777+LucasNogueiradeAzevedo-Dev@users.noreply.github.com>
2023-03-11 08:55:08 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import this
+
+num1 = 10
+num2 = 5
+
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+exponenciacao = num1 ** num2
+restoDe = num1 % num2
+
+print("soma:", soma)
+print("subtração:", subtracao)
+print("multiplicação:", multiplicacao)
+print("divisão:", divisao)
+print("exponenciação:", exponenciacao)
+print("restoDe:", restoDe)
+
+resultado = (10 + 3) * 7 / 2 - 3 ** 2
+print("Resultado:", resultado)
+
+num1 = 10
+num2 = 20
+num3 = 30
+
+media = (num1 + num2 + num3) / 3
+
+print("A média dos 3 são:", media)
+
+idade = 18
+ano = 2023
+altura = 1.75
+nome = "lucas"
+
+print(type(idade))
+print(type(ano))
+print(type(altura))
+print(type(nome))
